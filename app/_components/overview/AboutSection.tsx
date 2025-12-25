77a99ce85e360c47eb52beefdeec130a17ab01cc
aryndraa <aryacoder1102@gmail.com>
2025-12-25 19:50:24 +0800

    Large screen about

--- app/_components/overview/AboutSection.tsx
+++ app/_components/overview/AboutSection.tsx
@@ -11,8 +11,8 @@ export default function AboutSection() {
       className=" flex bg-background relative overflow-hidden lg:min-h-[80vh]"
     >
       <div className="2xl:container mx-auto px-4 md:px-8 lg:px-12 py-12 relative z-1">
-        <div className="mb-4 lg:absolute">
-          <Image src={AboutImage} alt="about-bg" />
+        <div className="mb-4 lg:absolute ">
+          <Image src={AboutImage} alt="about-bg" className=" 2xl:w-[60vw]" />
         </div>
         <div className="flex flex-col lg:items-end lg:justify-end h-full relative z-1 ">
           <div className="bg-foreground p-4 md:p-6 lg:p-8 mb-4 lg:w-[50%] shadow">
@@ -39,12 +39,12 @@ export default function AboutSection() {
       <Image
         src={AboutRightDecor}
         alt="about-bg"
-        className="absolute saturate-0 -top-32 -right-28 lg:-right-16 rotate-60 lg:rotate-70 opacity-60"
+        className="absolute saturate-0 -top-32 2xl:-top-12 -right-28 lg:-right-16 2xl:-right-8 rotate-60 lg:rotate-70 opacity-60"
       />
       <Image
         src={AboutLeftDecor}
         alt="about-bg"
-        className="absolute saturate-0 -bottom-40 lg:-bottom-24  -left-40 rotate-140 opacity-60"
+        className="absolute saturate-0 -bottom-40 2xl:-bottom-12 lg:-bottom-24  -left-40 2xl:-left-24 lg rotate-140 opacity-60"
       />
     </section>
   );
