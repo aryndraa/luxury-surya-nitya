76967b9903c3c065dcdb6e607761b83f7bd7c6d5
aryndraa <aryacoder1102@gmail.com>
2025-12-29 17:28:37 +0800

    Offers booking section

---
+++ app/_components/offers/sections/BookingSection.tsx
@@ -0,0 +1,172 @@
+"use client";
+import React, { useState } from "react";
+import CheckAvailabilityBg from "@/public/assets/home/check-availability-bg.jpg";
+import AboutRightDecor from "@/public/assets/home/faq-flower-r.svg";
+import AboutLeftDecor from "@/public/assets/home/faq-flower-l.svg";
+import Image from "next/image";
+import HeadingLine from "../../HeadingLine";
+
+type BookingForm = {
+  name: string;
+  phone: string;
+  package: string;
+  guest: number;
+  reservationDate: string;
+};
+
+export default function BookingSection() {
+  const [form, setForm] = useState<BookingForm>({
+    name: "",
+    phone: "",
+    package: "",
+    guest: 1,
+    reservationDate: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(form);
+
+    // nanti bisa:
+    // - kirim ke API
+    // - redirect ke WhatsApp
+  };
+
+  return (
+    <section id="BookingForm" className="relative">
+      <div className=" absolute  bottom-0 top-0 h-full w-full  overflow-hidden  right-0 bg-white ">
+        <div className="relative h-full">
+          <Image
+            src={AboutRightDecor}
+            alt="about-bg"
+            className="absolute saturate-0 w-52 md:w-64 lg:w-96 top-36 lg:top-auto   lg:bottom-0 -right-20 lg:-right-28 2xl:-right-8   opacity-40"
+          />
+          <Image
+            src={AboutLeftDecor}
+            alt="about-bg"
+            className="absolute saturate-0 w-52 md:w-64 lg:w-96 top-36 lg:top-auto  lg:bottom-0  -left-20 2xl:-left-24  opacity-40"
+          />
+        </div>
+      </div>
+
+      <div className="2xl:container mx-auto px-4 md:px-8 lg:px-12 py-12 md:py-18 lg:py-24 relative z-1 flex flex-col items-center justify-center ">
+        <div className="mb-8 md:mb-12">
+          <HeadingLine>
+            <div className="flex flex-col items-center text-center">
+              <h1 className=" text-center text-3xl md:text-5xl font-playfair-display font-semibold mb-4 lg:mb-6 lg:text-nowrap">
+                Book Your Private Escape
+              </h1>
+              <p className="text-sm md:text-base font-medium lg:font-normal">
+                Reserve your stay at our exclusive villa and experience privacy,
+                comfort, and timeless serenity in Karangasem.
+              </p>
+            </div>
+          </HeadingLine>
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="bg-text-dark shadow-lg p-6 md:p-8 w-full lg:w-[60%] "
+        >
+          <h1 className="md:col-span-2 text-center text-3xl md:text-5xl font-playfair-display text-text-light font-semibold mb-8 md:mb-14 ">
+            Check Availability
+          </h1>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6 text-sm md:text-base">
+            <input
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              required
+              className="border-b border-text-light/40 focus:border-text-light pb-4  text-text-light placeholder:text-text-light/70 bg-transparent focus:outline-none transition ease-in-out"
+              placeholder="Your name"
+            />
+
+            <input
+              type="tel"
+              name="phone"
+              value={form.phone}
+              onChange={handleChange}
+              required
+              className="border-b border-text-light/40 focus:border-text-light pb-4  text-text-light placeholder:text-text-light/70 bg-transparent focus:outline-none transition ease-in-out"
+              placeholder="Phone Number"
+            />
+
+            <select
+              name="package"
+              value={form.package}
+              onChange={handleChange}
+              required
+              className={`border-b pb-4 bg-transparent focus:outline-none transition ease-in-out
+                ${
+                  form.package
+                    ? "text-text-light border-text-light"
+                    : "text-text-light/70 border-text-light/40"
+                }
+            `}
+            >
+              <option value="" disabled>
+                Select package
+              </option>
+              <option value="romantic" className="text-text-dark">
+                Romantic Escape
+              </option>
+              <option value="family" className="text-text-dark">
+                Family Getaway
+              </option>
+              <option value="honeymoon" className="text-text-dark">
+                Honeymoon Package
+              </option>
+            </select>
+
+            <div className="flex border-b pb-4 border-text-light/40 focus-within:border-text-light transition-colors duration-300">
+              <label className="text-text-light/70 mr-2">Guests :</label>
+
+              <input
+                type="number"
+                name="guest"
+                value={form.guest}
+                min={1}
+                onChange={handleChange}
+                required
+                className="text-text-light placeholder:text-text-light/70 bg-transparent focus:outline-none"
+              />
+            </div>
+
+            <div className="flex items-center gap-2 border-b pb-4 border-text-light/40 focus-within:border-text-light transition-colors duration-300 md:col-span-2">
+              <label className="text-text-light/70 whitespace-nowrap">
+                Reservation Date :
+              </label>
+              <input
+                type="date"
+                name="reservationDate"
+                value={form.reservationDate}
+                onChange={handleChange}
+                required
+                className="w-full text-text-light bg-transparent placeholder:text-text-light/70 focus:outline-none"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="md:col-span-2 mt-4 bg-primary text-white py-3 font-semibold rounded hover:opacity-90 transition"
+            >
+              Check Availability
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
--- app/offers/page.tsx
+++ app/offers/page.tsx
@@ -3,6 +3,9 @@ import TitleSection from "../_components/TitleSection";
 import HeroBg from "@/public/assets/offers/hero.jpg";
 import SpecialOfferSection from "../_components/offers/sections/SpecialOfferSection";
 import OffersSection from "../_components/offers/sections/OffersSection";
+import BookingSection from "../_components/offers/sections/BookingSection";
+import CatchEye from "../_components/CatchEye";
+import CatchEyeImage from "@/public/assets/offers/catch-eye.jpg";
 
 export default function Page() {
   return (
@@ -10,6 +13,8 @@ export default function Page() {
       <TitleSection image={HeroBg} title="Offers" />
       <SpecialOfferSection />
       <OffersSection />
+      <CatchEye image={CatchEyeImage} />
+      <BookingSection />
     </>
   );
 }
